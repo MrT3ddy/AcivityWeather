754baf31e1b8ab45237774a1d188159b53c08999
t3ddy <46868432+MrT3ddy@users.noreply.github.com>
2023-04-18 22:46:33 -0400

    [Altered requeset to include a promise on Activities / Also added UI for API loading] (kept setTimeout)

--- src/views/ActivityQuote.vue
+++ src/views/ActivityQuote.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="MoviewView">
+
     <h1>Activity with Quote</h1>
+
       <!-- React based display for API response !-->
-      <b-list-group>
+      <b-list-group v-if="activityLoaded">
         <b-list-group-item>Activity: {{ activity }} </b-list-group-item>
         <b-list-group-item>Kanye says "{{ quote2 }}" </b-list-group-item>
       </b-list-group>
+
+      <div v-else>
+        <b-spinner label="Loading..."> Loading . . . </b-spinner>
+      </div>
 
        <!-- Link to vue router for Weather !-->
       Outdoor activity? Check the weather!
@@ -16,13 +22,15 @@
 
 
 <style scoped>
+
 /* For title [h1] */
 h1 {
   text-decoration: underline;
 }
 </style>
-<script>
 
+
+<script>
 
 import axios from 'axios'
 export default{
@@ -30,8 +38,9 @@ export default{
     data() {
       return{
         // holds variables for site
-      activity: ' ',
-      quote2: ''
+      activity: '',
+      quote2: '',
+      activityLoaded: false
     }},
 
     // individual methods that request from API's
@@ -39,11 +48,10 @@ export default{
 
       // request for Activity
       async getActivity () {
+
         const URL = "https://www.boredapi.com/api/activity"
-        await axios.get(URL)
-        .then ( res => {
-          this.activity = res.data.activity;
-        })
+        const res = await axios.get(URL)
+
         .catch((error) => { // error is handled in catch block
           if (error.response) { // status code out of the range of 2xx
             console.log("Data :" , error.response.data);
@@ -53,16 +61,24 @@ export default{
           } else {// Error on setting up the request
             console.log('Error', error.message);
           }
+        })
+
+        return new Promise(resolve => {
+
+          setTimeout(() => {
+            this.activity = res.data.activity;
+            this.activityLoaded=true;
+            resolve();
+          }, 3000);
         })
       },
 
       // request for Quote
       async getQuote () {
-        const URL = "https://api.kanye.rest/"
-        await axios.get(URL)
-        .then ( res => {
-          this.quote2 = res.data.quote;
-        })
+
+        const URL = "https://api.kanye.rest/";
+        const res = await axios.get(URL)
+
         .catch((error) => { // error is handled in catch block
           if (error.response) { // status code out of the range of 2xx
             console.log("Data :" , error.response.data);
@@ -72,12 +88,15 @@ export default{
           } else {// Error on setting up the request
             console.log('Error', error.message);
           }
-        })}
+        })
+        
+        this.quote2 = res.data.quote;
 
+      }
     },
     // calling methods in the mounted hook
-    mounted() {
-      this.getActivity();
+    async mounted() {
+      await this.getActivity();
       this.getQuote();
     }
 }
